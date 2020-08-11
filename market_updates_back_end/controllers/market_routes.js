80c371cbe03a42b0535be866249f430aace55311
Aaron Marino <aaron_marino@yahoo.com>
2020-08-11 11:39:29 -0400

    routes created for market_routes.js

--- market_updates_back_end/controllers/market_routes.js
+++ market_updates_back_end/controllers/market_routes.js
@@ -15,6 +15,35 @@ markets.post('/', async (req, res) => {
         res.status(200).send(createdStock)
     })
 })
+// Index Route
+markets.get('/', (req, res) => {
+    Stocks.find({}, (err, foundStocks) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        res.status(200).json(foundStocks)
+    })
+})
+
+// Delete Route
+markets.delete('/:id', (req, res) => {
+    Stocks.findByIdAndRemove(req.params.id, (err, deletedStock) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        res.status(200).json(deletedStock)
+    })
+})
+
+// Update Route
+markets.put('/:id', (req, res) => {
+    Stocks.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedStock) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        res.send(200).json({updatedStock})
+    })
+})
 
 
 module.exports = markets
