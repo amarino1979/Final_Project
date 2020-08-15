1b39a6f7404e7bcd55e98bbc6eb2b1d9282c660f
Aaron Marino <aaron_marino@yahoo.com>
2020-08-14 23:19:23 -0400

    fetch statement for app.js

--- market_front_end/src/App.js
+++ market_front_end/src/App.js
@@ -13,6 +13,12 @@ export default class App extends Component {
       searchURL: ''
     }
   }
+  componentDidMount() {
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
   //fetch from the back end
 
   render() {
