7b99413f63565ee862dd41749e805474f8f4cb3e
Aaron Marino <aaron_marino@yahoo.com>
2020-08-17 22:40:02 -0400

    updating MONGODB_URI connection logic with heroku

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 5000
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `YOUR DATABASE NAME`;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/markets';
 
-mongoose.connect('mongodb://localhost:27017/markets', { 
+mongoose.connect(MONGODB_URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
 })
