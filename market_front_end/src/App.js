bdc256818dc0be30c769822b194eb3e2a984eb63
Aaron Marino <aaron_marino@yahoo.com>
2020-08-17 20:15:39 -0400

    added components directory, added stockinfo.js, dynamic search functioning, added background-image

--- market_front_end/src/App.js
+++ market_front_end/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-
-console.log(process.env)
+// import './style.css'
+import './App.css'
+import SearchBar from './components/SearchBar'
+import StockInfo from './components/StockInfo'
 
 export default class App extends Component {
   constructor (props) {
@@ -8,25 +10,53 @@ export default class App extends Component {
     this.state = {
       baseURL: 'https://www.alphavantage.co/query?',
       function: 'function=GLOBAL_QUOTE&',
-      symbol: '',
       apikey: '',
-      searchURL: ''
+      currentValue: '',
+      symbolList: [],
+      stocks: []
     }
   }
 
-  handleChange = (event) => {
-    console.log(event.target)
-    this.setState({ [event.target.id]: 'symbol=' + event.target.value })
+  handleChange = async (event) => {
+    console.log(event.target.value)
+    this.setState({currentValue: event.target.value})
+    const keyword = 'keywords=' + event.target.value
+    const symbolfunction = 'function=SYMBOL_SEARCH&'
+    
+    const searchURL = this.state.baseURL + symbolfunction + keyword + this.state.apikey
+    const symList = []
+    await fetch(searchURL)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.bestMatches){
+          data.bestMatches.map(match => symList.push({name: match['2. name'], symbol: match['1. symbol']}))
+        }
+      })
+      this.setState({ symbolList: symList})
+      
     console.log(this.state)
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault()
-    const searchURL = this.state.baseURL + this.state.function + this.state.symbol + this.state.apikey
-    console.log(searchURL)
-    fetch(searchURL)
+    const symbol = 'symbol=' + this.state.currentSymbol
+    const searchURL = this.state.baseURL + this.state.function + symbol + this.state.apikey
+    await fetch(searchURL)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({stocks: [...this.state.stocks, {
+        name: this.state.currentValue, 
+        symbol: data['Global Quote']['01. symbol'], 
+        open: data['Global Quote']['02. open'],
+        high: data['Global Quote']['03. high'],
+        low: data['Global Quote']['04. low'],
+        price: data['Global Quote']['05. price'],
+        volume: data['Global Quote']['06. volume'],
+        latestTradingDay: data['Global Quote']['07. latest trading day'],
+        previousClose: data['Global Quote']['08. previous close'],
+        change: data['Global Quote']['09. change'],
+        changePercent: data['Global Quote']['10. change percent'],
+      }]}))
+      console.log(this.state)
   }
 
   componentDidMount = async() => {
@@ -36,27 +66,48 @@ export default class App extends Component {
       .then(async (data) => {
         await this.setState({apikey: '&apikey=' + data})
       }) 
-      console.log('This is your data', this.state);
   }
 
-
+  handleSelectClick = (name, symbol) => {
+    this.setState({currentValue: name, symbolList: [], currentSymbol: symbol})
+  }
 
   render() {
+    const tickerOptions = this.state.symbolList
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor='symbol'>Ticker</label>
-          <input 
-          id="symbol"
-          type="text"
-          value={this.state.stockTicker}
-          onChange={this.handleChange}
-          />
-          <input
-            type="submit"
-            value="search"
+      <div className="container">
+        <form className="search_form">
+          <div>
+          <label htmlFor='keywords'>Ticker</label>
+          </div>
+          <div className="search">
+            <input
+              placeholder={'Select stock..'}
+              value={this.state.currentValue || null}
+              onChange={this.handleChange}
             />
+            {tickerOptions.length > 0 &&
+              <select value={tickerOptions.name} size={tickerOptions.length}>
+                {
+                tickerOptions.map((option_) => {
+                  return <option onClick={() => this.handleSelectClick(option_.name, option_.symbol)} value={option_.name}>
+                    ${option_.name}
+                  </option>
+                })
+              }
+              </select>
+            }
+          </div>
+          <div>
+          <button onClick={this.handleSubmit}
+            type="submit"
+            // value="search"
+            >search</button>
+            </div>
         </form>
+        {this.state.stocks.length > 0 &&
+        <StockInfo stock={this.state.stocks}/> 
+        }
       </div>
     )
   }
