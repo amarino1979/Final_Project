45f357448f4a68d171e0acebc6be34af5ad1819f
Aaron Marino <aaron_marino@yahoo.com>
2020-08-17 21:37:35 -0400

    split up front and backend so that I can deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 require('dotenv').config()
 //const fetch = require(node-fetch)
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 
 
@@ -13,8 +13,12 @@ const PORT = 5000
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-mongoose.connect('mongodb://localhost:27017/markets', { useNewUrlParser: true,
-useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `YOUR DATABASE NAME`;
+
+mongoose.connect('mongodb://localhost:27017/markets', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose')
 })
